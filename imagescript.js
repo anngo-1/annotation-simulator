60b5c437e6e71d568860d04990aca4b8f6ac88a6
An Ngo <andngo2004@gmail.com>
2023-04-07 17:03:45 -0700

    back button

--- imagescript.js
+++ imagescript.js
@@ -1,6 +1,7 @@
 const input = document.getElementById('folder');
 const container = document.querySelector('.image-container');
 const nextBtn = document.getElementById('next-btn');
+const backBtn = document.getElementById("back-btn")
 const download = document.getElementById('downloadbutton')
 let jsonData = []
 let currentName = ""
@@ -41,22 +42,69 @@ nextBtn.addEventListener('click', () => {
     //clear the checkbox
     checkboxes[i].checked = false;
   }
+  // first, check if a label has already been made. overwrite if so
+
+  const checkLabel = obj => obj.file_name === currentName;
+  
+  if (jsonData.some(checkLabel)) //if already labeled, overwrite
+  {
+  
+    jsonData[currentFileIndex-1] =
+    {  
+      file_name:currentName,  
+      tags_labeled: checkedtags,
+      file_num: currentFileIndex
+    }
+    
+  } else {
 
 
+  // case for completely new
+  
   jsonData.push(
 
     {  
     file_name:currentName,  
     tags_labeled: checkedtags,
-    file_num: currentFileIndex+1
+    file_num: currentFileIndex
   }
   
 
   )
-
-  console.log(jsonData)
+  }
 
 });
+
+
+
+backBtn.addEventListener('click', () => {
+  const checkboxes = document.querySelectorAll('input[type="checkbox"]'); // redefine 
+  // go back an index
+  if (currentFileIndex!=0) {
+    currentFileIndex-=1;
+    displayImage(currentFileIndex)
+  
+
+  currentArray = jsonData[currentFileIndex]
+  for (var i = 0; i < checkboxes.length; i++) {
+
+      //get the checkbox's id (tag)
+      currentArrayTags = currentArray.tags_labeled
+      if (currentArrayTags.includes(checkboxes[i].id)) {
+        checkboxes[i].checked = true;
+      }
+  
+    }
+  
+  }
+
+})
+
+
+
+
+
+
 
 // adding all of the files uploaded to an array
 input.addEventListener('change', (event) => {
@@ -71,11 +119,21 @@ input.addEventListener('change', (event) => {
 
 // function for displaying an image
 function displayImage(index) {
+
+    // if the image is already labeled (for example we are going back), show its tags
+  
+
+
+
+
+
     file = files[index]
     currentName = file.name
     const breakline = document.createElement("br")
     const image = document.createElement('img');
     const count = document.createTextNode("Image " + (currentFileIndex+1))
+    image.style.width = "50%"
+    
     image.onload = () => {
       container.innerHTML = '';
       container.appendChild(count)
