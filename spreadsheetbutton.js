0a6e0cbab06d78cb562fb6f850a2e463ac664d89
An Ngo <andngo2004@gmail.com>
2023-04-14 09:41:38 -0700

    remove local api

--- spreadsheetbutton.js
+++ spreadsheetbutton.js
@@ -1,5 +1,8 @@
 const sendbutton = document.getElementById("sendtosheet")
+const resultbutton = document.getElementById("getresults")
 const link = document.getElementById("sheetlink")
+const results = document.getElementById("results")
+const tagstext = document.getElementById("tagstext")
 sendbutton.onclick = function(){
 
     if (link.value != "" ){
@@ -7,7 +10,7 @@ sendbutton.onclick = function(){
         "link":link.value,
         "data": jsonData
     }
-        fetch('http://127.0.0.1:5002/updatesheet', {
+        fetch('https://anngo1.pythonanywhere.com/updatesheet', {
             method: 'POST',
             body: JSON.stringify(data),
             headers: {
@@ -17,6 +20,27 @@ sendbutton.onclick = function(){
           .then(response => response.json())
           .then(data => console.log(data))
           .catch(error => console.error(error));
+    } else {
+        alert("please put in a google sheet")
+    }
+
+}
+
+
+
+resultbutton.onclick = function() {
+    if (link.value != "" ){
+    const params = new URLSearchParams({
+        link:link.value,
+        tags_used: tagstext.value
+        });
+        fetch(`http://anngo1.pythonanywhere.com/sheetstats?${params}`)
+        .then(response => response.json())
+        .then(data => results.innerHTML = "Tags Used: " + data["tags_used"] + "\n" + "Annotator agreement (by percentage): " +  data["agreement_percentage"] + "\n" + "Images agreed upon: " + data["total_agreement_images"])
+        .catch(error => {
+            alert(error)
+            
+            console.error(error)});
     } else {
         alert("please put in a google sheet")
     }
